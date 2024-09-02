96b012c517e8f7e2bf35ab83b81390f0279153a7
Whit Nelson <whitnelson@gmail.com>
2024-09-02 13:05:57 -0400

    update to maths

--- docs/js/app.js
+++ docs/js/app.js
@@ -16,6 +16,7 @@ const quizApp = {
     currentFrame: 0,
 
     init: function () {
+        this.injectHtml();
         this.initHooks();
         this.loadQuizData();
     },
@@ -327,9 +328,220 @@ const quizApp = {
             this.blockData[block].items.forEach((item) => {
                 total += item.priority;
             });
+            // Round
+
             this.blockData[block].average = total / this.blockData[block].items.length;
         }
 
         console.log(this.blockData);
     },
+
+    injectHtml: function () {
+        let html = `      
+        <section class="quiz">
+            <div data-quiz-container class="quiz__container">
+                <!-- Start page -->
+                <div data-template-start>
+                    <div class="quiz__layout quiz__layout--start">
+                    <div class="quiz__header">
+                        <h1 class="headline">Innovator Quiz</h1>
+                        <p>
+                        Assess your strengths and weaknesses based on Innovation
+                        Building Blocks.
+                        </p>
+                    </div>
+                    <div class="quiz__body">
+                        <div class="">
+                        <h2>Innovation Building Blocks</h2>
+                        <p>
+                            The Innovator Building Blocks consist of 40 essential
+                            activities identified by innovators during the Compass Pilot
+                            co-design process. Inspired by the real journeys of other
+                            early-stage innovators, the building blocks seek to
+                            demystify the often challenging and ambiguous experience of
+                            starting your own company. The building block activities aim
+                            to foster a comprehensive understanding of what it takes to
+                            take a budding idea and transform it into a sustainable
+                            venture by providing guidance on essential topics such as
+                            vision setting, sustainable funding, business operations,
+                            and more.
+                        </p>
+                        <button data-start-button>Start Quiz</button>
+                        <button data-finish-button>Finish Quiz</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+                <!-- Title page -->
+                <div data-template-title class="hidden">
+                    <div class="quiz__layout quiz__layout--title">
+                    <div class="quiz__header">
+                        <h1 data-quiz-title-headline class="headline">
+                        Innovator Quiz
+                        </h1>
+                    </div>
+                    <div class="quiz__body">
+                        <div class="quiz__progress"></div>
+                        <div class="quiz__card-wrap">
+                        <div class="quiz__card">
+                            <h2 data-title-headline class="headline"></h2>
+                            <p data-title-blurb class="blurb"></p>
+                        </div>
+                        <div class="quiz__buttons">
+                            <button data-prev-button class="quiz__button">
+                            Previous
+                            </button>
+                            <button data-next-button class="quiz__button">Next</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+                <!-- Question page -->
+                <div data-template-question class="hidden">
+                    <div class="quiz__layout quiz__layout--question">
+                    <div class="quiz__header">
+                        <h1 data-quiz-question-headline class="headline">
+                        Innovator Quiz
+                        </h1>
+                    </div>
+                    <div class="quiz__body">
+                        <div class="quiz__progress">
+                        <div class="quiz__progress-text">
+                            <div class="quiz__progress-text__inner">
+                            <div>
+                                <strong>Category</strong>
+                            </div>
+                            <div>
+                                <span data-category-body></span>
+                                (<span data-category-number></span>)
+                            </div>
+                            </div>
+                            <div>
+                            <strong>
+                                <span data-progress-percent></span>
+                                complete</strong
+                            >
+                            </div>
+                        </div>
+                        <!-- progress bar -->
+                        <div class="quiz__progress-bar">
+                            <div
+                            class="quiz__progress-bar__fill"
+                            data-progress-bar-fill
+                            ></div>
+                        </div>
+                        </div>
+                        <div class="quiz__card-wrap">
+                        <div class="quiz__card">
+                            <p class="question">
+                            How prepared are you or your team to accomplish the
+                            following?
+                            </p>
+                            <div class="">
+                            <h2 data-question-headline class="headline"></h2>
+                            <div class="hidden">
+                                <div data-question-blurb class="blurb"></div>
+                            </div>
+                            </div>
+                            <div class="option-group">
+                            <div class="option">
+                                <input
+                                type="radio"
+                                id="option1"
+                                name="answer"
+                                value="0"
+                                data-answer
+                                data-answer-0
+                                />
+                                <label for="option1">Unprepared</label>
+                            </div>
+                            <div class="option">
+                                <input
+                                type="radio"
+                                id="option2"
+                                name="answer"
+                                value="1"
+                                data-answer
+                                data-answer-1
+                                />
+                                <label for="option2">Somewhat Prepared</label>
+                            </div>
+                            <div class="option">
+                                <input
+                                type="radio"
+                                id="option3"
+                                name="answer"
+                                value="2"
+                                data-answer
+                                data-answer-2
+                                />
+                                <label for="option3">Adequately Prepared</label>
+                            </div>
+                            <div class="option">
+                                <input
+                                type="radio"
+                                id="option4"
+                                name="answer"
+                                value="3"
+                                data-answer
+                                data-answer-3
+                                />
+                                <label for="option4">Very Prepared</label>
+                            </div>
+                            <div class="option">
+                                <input
+                                type="radio"
+                                id="option5"
+                                name="answer"
+                                value="4"
+                                data-answer
+                                data-answer-4
+                                />
+                                <label for="option5">Extremely Prepared</label>
+                            </div>
+                            </div>
+                            <div
+                            data-error-message
+                            class="error error--no-selection hidden"
+                            >
+                            <p>Please make a selection from the options above</p>
+                            </div>
+                        </div>
+                        <div class="quiz__buttons">
+                            <button data-prev-button class="quiz__button">
+                            Previous
+                            </button>
+                            <button data-next-button class="quiz__button">Next</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+                <!-- Results page -->
+                <div data-template-results class="hidden">
+                    <div class="quiz__layout quiz__layout--results">
+                    <div class="quiz__header">
+                        <h1 class="headline">Results</h1>
+                        <p>Thank you for taking the quiz.</p>
+                    </div>
+                    <div class="quiz__body">
+                        <div class="">
+                        <p>Data results</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        `;
+        // Inject the HTML
+        let scriptNode = document.querySelector("[data-quiz-script]");
+        var htmlNode = document.createElement('div');
+        htmlNode.innerHTML = html;
+        scriptNode.after(htmlNode);
+    }
 };
