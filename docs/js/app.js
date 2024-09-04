4eba1253a2d67c759a45963573d99b96418a34c3
Will Garrison <willgarrison@gmail.com>
2024-09-04 14:21:02 -0400

    adding tooltip

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
 	quizApp.init();
-	window.addEventListener("beforeunload", (event) => {
-		// Cancel the event as stated by the standard.
-		event.preventDefault();
-		// Chrome requires returnValue to be set.
-		event.returnValue = "";
-	});
+	// window.addEventListener("beforeunload", (event) => {
+	// 	// Cancel the event as stated by the standard.
+	// 	event.preventDefault();
+	// 	// Chrome requires returnValue to be set.
+	// 	event.returnValue = "";
+	// });
 });
 
 const quizApp = {
